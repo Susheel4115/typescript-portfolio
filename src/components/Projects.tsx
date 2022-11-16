a1dbe8fb6889ed67345a9f5e6884957cdc824d85
Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2022-11-16 11:40:59 +0530

    Updated project.tsx for alignment

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ export default function Projects(props: any) {
   return (
     <section
       id="projects"
-      className="text-gray-400 bg-gray-900 font-mono body-font"
+      className="text-gray-400 font-mono bg-gray-900 body-font"
     >
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
@@ -25,12 +25,12 @@ export default function Projects(props: any) {
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-1/2 w-100 p-1 m-1"
+              className="sm:w-1/2 w-100 p-4"
             >
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
