c6f3862d4c75eccc75e9b018e58ebd11c3550e2e
Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2022-11-19 12:11:55 +0530

    updated contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -63,7 +63,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">+91 939-868-9812</p>
+              <p className="leading-relaxed">+91 939-104-7937</p>
             </div>
           </div>
         </div>
