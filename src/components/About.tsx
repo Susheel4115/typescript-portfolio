2250352ca6c01f8bcda5cf3f3fc9f0193c2f136e
Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2023-02-20 22:01:14 +0530

    added resume nav link and cleared some css

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,40 +1,50 @@
 import React from "react";
 // import { CameraIcon } from "./profile/User";
-import profile from "./profile/profile-pic.png"
+import profile from "./profile/profile-pic.png";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto font-mono flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container mx-auto font-mono flex px-10 py-20 md:flex-row flex-col items-center mt-20">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm <span className="font-serif text-emerald-400">Susheel 🤪</span>.
+            Hi, I'm{" "}
+            <span className="font-serif text-emerald-400">Susheel 🤪</span>.
             <br className="hidden lg:inline-block" />I love to build amazing
             apps and do solving questions in leetcode😁.
           </h1>
           <p className="mb-8 leading-relaxed">
-            <span className="text-pink-700">A motivated individual who is good at problem-solving and
-            programming, with an emphasis on writing clean and maintainable
-            code. Experienced with</span> <span className="text-emerald-600">data structures/algorithms and designing
-            optimal solutions, aspiring to make a career in software engineering
-            with a focus on full-stack development.</span>
+            <span className="text-purple-500">
+              A motivated individual who is good at problem-solving and
+              programming, with an emphasis on writing clean and maintainable
+              code. Experienced with
+            </span>{" "}
+            <span className="text-emerald-600">
+              data structures/algorithms and designing optimal solutions,
+              aspiring to make a career in software engineering with a focus on
+              full-stack development.
+            </span>
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-black font-mono text-center bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-300 rounded-sm  text-sm hover:rotate-5 transition duration-1000  ease-in-out"
+              className="inline-flex text-black font-mono text-center bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 hover:text-white rounded-2xl  text-sm hover:rotate-5 transition duration-1000  ease-in-out"
             >
               Work With Me
             </a>
             <a
               href="#projects"
-              className="ml-4 inline-flex text-gray-400 font-mono bg-pink-800 border-0 py-2 px-8 focus:outline-none hover:bg-pink-500 hover:text-white rounded-sm text-sm hover:rotate-5 transition duration-1000  ease-in-out"
+              className="ml-4 inline-flex text-black font-mono bg-purple-400 border-0 py-2 px-8 focus:outline-none hover:bg-purple-800 hover:text-white rounded-2xl text-sm hover:rotate-5 transition duration-1000  ease-in-out"
             >
               See My Past Work
             </a>
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 hidden md:block">
-          <img src={profile} alt ="pic" className="rounded-full object-contain h-96 w-fit hover:rotate-360 transition duration-1000  ease-in-out cursor-pointer"/>
+          <img
+            src={profile}
+            alt="pic"
+            className="rounded-full object-contain h-96 w-fit hover:rotate-360 transition duration-1000  ease-in-out cursor-pointer"
+          />
           {/* <a href="" */}
         </div>
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,31 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
-
+import { useState } from "react";
 export default function Navbar() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleHideModal = () => {
+    setShowModal(false);
+  };
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10 font-mono">
+    <header className="fixed bg-gray-800 sm:sticky top-0 z-10 font-mono">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a
           className="title-font font-medium text-white mb-4 md:mb-0"
           href="#about"
         >
-          
-          <a href="#about" className="ml-3 text-xl text-blue-100 hover:text-white cursor-pointer">
+          <a
+            href="#about"
+            className="ml-3 text-xl text-blue-100 hover:text-white cursor-pointer"
+          >
             Susheel Kumar
           </a>
-          
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center md:justify-evenly ">
+          <a href="#projects" className="mr-5 hover:text-white sm:mt-1">
             Past Work
           </a>
           <a href="#skills" className="mr-5 hover:text-white">
@@ -24,6 +34,33 @@ export default function Navbar() {
           <a href="#testimonials" className="mr-5 hover:text-white">
             Competative_Programming
           </a>
+          <a
+            href="#resume"
+            onClick={handleShowModal}
+            className="mr-5 hover:text-white"
+          >
+            Resume
+          </a>
+          {showModal && (
+            <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 z-50 flex items-center justify-center">
+              <div className="bg-white rounded-lg p-6 max-w-md mx-auto">
+                <h2 className="text-xl font-bold mb-4">Resume</h2>
+                <p>
+                  Please drop me the requirements you're looking for in the
+                  contact form(you can find when you click hire me link in the
+                  navbar) with your gmail. So that I can get a mail from you and
+                  I will reply back to that mail with my resume and cover
+                  letter.💌
+                </p>
+                <button
+                  className="bg-green-800 text-white rounded p-1 pr-5 pl-5   ml-1 mt-4"
+                  onClick={handleHideModal}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          )}
         </nav>
         <a
           href="#contact"
